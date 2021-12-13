0e0f2cc27254c02f092bfbbe63529e1028e42790
jonnovaretti <jonathan.novaretti@freshbooks.com>
2021-12-13 11:14:17 -0300

    Setting up social media authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth', sessions: 'users/sessions'}
 end
