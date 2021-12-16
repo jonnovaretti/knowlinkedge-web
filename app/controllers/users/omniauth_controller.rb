f12f48d902e63acfa4e2213357635f2f22fe0b66
jonnovaretti <jonathan.novaretti@freshbooks.com>
2021-12-15 22:11:36 -0300

    Refactoring

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -2,56 +2,31 @@ require 'devise'
 
 class Users::OmniauthController < Users::OmniauthCallbacksController
     def facebook
-      @user = User.create_from_provider_data(request.env['omniauth.auth'])
-      if @user.persisted?
-          sign_in_and_redirect @user
-          set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-      else
-          flash[:error] = 'There was a problem signing you in through Facebook. Please register or try signing in later.'
-          redirect_to new_user_registration_url
-      end
+      authenticate_social_media('Facebook')
     end
 
     def github
-      @user = User.create_from_provider_data(request.env['omniauth.auth'])
-      if @user.persisted?
-        sign_in_and_redirect @user
-        set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
-      else
-        flash[:error] = 'There was a problem signing you in through GitHub. Please register or try signing in later.'
-        redirect_to new_user_registration_url
-      end
+      authenticate_social_media('GitHub')
     end
 
     def google_oauth2
-      p '111111111111111111111111111111111111111111'
-      @user = User.create_from_provider_data(request.env['omniauth.auth'])
-      p '22222222222222222222222222222222222222222222'
-      if @user.persisted?
-        p '3333333333333333333333333333333333333333333'
-        sign_in_and_redirect @user
-        p '444444444444444444444444444444444444444444444'
-        set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
-      else
-        p '555555555555555555555555555555555555555555555'
-        flash[:error] = 'There was a problem signing you in through Google. Please register or try signing in later.'
-        redirect_to new_user_registration_url
-      end
+      authenticate_social_media('Google')
     end
     
     def twitter
+      authenticate_social_media('Twitter')
+    end
+
+    private
+
+    def authenticate_social_media (social_media)
       @user = User.create_from_provider_data(request.env['omniauth.auth'])
       if @user.persisted?
         sign_in_and_redirect @user
-        set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
+        set_flash_message(:notice, :success, kind: social_media) if is_navigational_format?
       else
-        flash[:error] = 'There was a problem signing you in through Twitter. Please register or try signing in later.'
+        flash[:error] = "There was a problem signing you in through #{social_media}. Please register or try signing in later."
         redirect_to new_user_registration_url
       end
-    end
-
-    def failure
-      flash[:error] = 'There was a problem signing you in. Please register or try signing in later.'
-      redirect_to new_user_registration_url
     end
 end
